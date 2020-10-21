fdaa2fb4e8697806230fdd758920a0f3f317fc69
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-21 16:01:08 -0400

    oof

--- src/commands/remove-role.js
+++ src/commands/remove-role.js
@@ -6,7 +6,21 @@ module.exports = {
 	description: "Removes a person from the bot role",
 	usage: ['(command)'],
 	execute(message, args) {
-    message.mentions
+		const member = message.mentions.members.first();
+    //const role = role thing;
 
+
+   if(member.roles.has(role))
+      {
+
+      await(message.member.removeRole(role));
+
+      try{
+        console.log(`removed role from ${member}`);
+      } catch(e){
+
+      }
+
+ }
 	},
 };
