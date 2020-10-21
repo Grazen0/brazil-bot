8380fbae90b3c1040c8bd6ba6ccffb5021cef1d2
ElCholoGamer <josedanielgrayson@gmail.com>
2020-10-21 17:33:14 -0500

    check owner stuff

--- src/commands/currency/bestow.js
+++ src/commands/currency/bestow.js
@@ -4,19 +4,21 @@ const { asCurrency } = require('../../util/utils');
 module.exports = {
 	name: 'bestow',
 	description: '...',
-	execute: async ({ author, channel,  mentions, args, message }) => {
-    if (author.id == '675391536451682324' || author.id == '537486856384675852') {
-		const reward = parseInt(args[0]);
-    const member = message.mentions.users.first();
+	execute: async ({ author, channel, mentions, args, message }) => {
+		if (!config.owners.includes(author.id)) {
+			channel.send("You don't have permission for this!");
+			return;
+		}
 
-    if (!member) {
-      channel.send("pls mention a user")
-    }
+		const reward = parseInt(args[0]);
+		const member = message.mentions.users.first();
+
+		if (!member) {
+			channel.send('pls mention a user');
+			return;
+		}
 
 		await member.add(reward);
-		channel.send(
-			`${member} received \`${asCurrency(reward)}\``
-		);
-  }
+		channel.send(`${member} received \`${asCurrency(reward)}\``);
 	},
 };
