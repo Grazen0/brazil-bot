603ecc7a4883a0bdab84ef159857d36aaa268331
ElCholoGamer <josedanielgrayson@gmail.com>
2020-10-21 16:39:16 -0500

    epic

--- src/commands/currency/ticket.js
+++ src/commands/currency/ticket.js
@@ -1,15 +1,16 @@
 const config = require('../../../config.json');
 const { asCurrency } = require('../../util/utils');
-let ticketPrice = config.ticketPrice;
+
+const getPrice = () =>
+	config.ticketPrice *
+	(new Date().getDay() === 0 /* This is sunday btw */
+		? config.sundayDiscount
+		: 1);
 
 module.exports = {
 	name: 'ticket',
-	description: `Buy a ticket to brazil for ${asCurrency(ticketPrice)}!`,
-	execute: async ({message, args}) => {
-		const date = new Date();
-		const day = date.getDay();
-		if (day == 6) {
-			ticketPrice = 1500;
-		}
+	description: `Buy a ticket to brazil for ${asCurrency(getPrice())}!`,
+	execute: async ({ message, args }) => {
+		const price = getPrice();
 	},
 };
---
+++ src/commands/rick-roll.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+
+const config = require('../../config.json');
+
+module.exports = {
+	name: 'rule34',
+	description: '...',
+	usage: ['(tag1)', '(tag2)', '...'],
+	execute: async ({ channel }) => {
+		channel.send(
+			new MessageEmbed()
+				.setTitle('SIKE')
+				.setImage(
+					'https://media1.tenor.com/images/467d353f7e2d43563ce13fddbb213709/tenor.gif?itemid=12136175'
+				)
+				.setColor(config.embedColor)
+		);
+	},
+};
