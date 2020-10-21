515d784d143bed0155197bc4316da47555aa6ff1
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-21 14:36:41 -0400

    sry i had class all day

---
+++ src/commands/remove-role.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+	name: 'help',
+	description: "Shows this bot's command list.",
+	usage: ['(command)'],
+	execute: async ({ message, channel, client, args }) => {
+    
+
+	},
+};
