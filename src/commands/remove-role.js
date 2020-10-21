cefb6f7a65182112d7521edc2800e9b6a0ddb8ce
ElCholoGamer <josedanielgrayson@gmail.com>
2020-10-21 16:20:07 -0500

    remove role finished

--- src/commands/remove-role.js
+++ src/commands/remove-role.js
@@ -2,15 +2,38 @@ const config = require('../../config.json');
 
 module.exports = {
 	name: 'roleremove',
-	description: 'Removes a person from the Brazil role',
+	description: 'Removes a person from the Brazil role.',
 	usage: ['[@Member]'],
-	execute: async (message, args) => {
+	permissions: ['MANAGE_ROLES'],
+	execute: async ({ message, channel, client }) => {
 		const member = message.mentions.members.first();
-		const brazilRole = config;
+		const { brazilRole } = config;
 
-		if (member.roles.has(brazilRole)) {
-			await message.member.removeRole(brazilRole);
-			message.channel.send(`Removed role from ${member}`);
+		if (!member) {
+			channel.send('You need to mention a member!');
+			return;
+		}
+
+		const {
+			user: { tag },
+		} = member;
+
+		if (!member.roles.cache.get(brazilRole)) {
+			channel.send(`${tag} doesn\'t have the Brazil role!`);
+			return;
+		}
+
+		try {
+			await member.roles.remove(brazilRole);
+			await client.models.UserTickets.destroy({
+				where: { user_id: member.id },
+			});
+
+			channel.send(`Removed Brazil role from \`${tag}\`!`);
+		} catch (err) {
+			channel.send(
+				'An error ocurred while removing the role! This is most likely an issue with permissions.'
+			);
 		}
 	},
 };
