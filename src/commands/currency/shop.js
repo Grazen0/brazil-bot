6af909c535027cd90f0102fb161506d4a73f9eae
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-22 17:41:46 -0400

    buy a ticket

--- src/commands/currency/shop.js
+++ src/commands/currency/shop.js
@@ -11,7 +11,7 @@ module.exports = {
         .setTitle("Brazil Bot's shop")
         .setColor(config.embedColor)
         .setThumbnail(client.user.displayAvatarURL({ dynamic: true}))
-        .setDescription('Item shop is empty')
+        .setDescription(`Buy a ticket **$${config.ticketPrice}** \`${config.prefix}ticket\``)
         .setFooter(`do ${config.prefix}shop (page number)`)
     );
 	},
