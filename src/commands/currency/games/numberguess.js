1ba0295af1a134c34eae09f6d03e90fbdf22a228
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-23 16:46:49 -0400

    not finished

---
+++ src/commands/currency/games/numberguess.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require('discord.js');
+const config = require('../../../../config.json');
+const { asCurrency } = require('../../../util/utils');
+
+module.exports = {
+	name: 'number',
+	description: 'Earn more money, doubles your gamble',
+  usage: '(number)',
+	execute: async ({ channel, author, client, args }) => {
+		const bal = await author.getBalance();
+    const price = parseInt(args[0]);
+    const reward = price*3;
+
+    if (!args.length) {
+  		return message.channel.send(`You didn't mention your bid`);
+  	}
+
+		if (bal < price) {
+			channel.send(
+				`You don't have enough money lol, you need ${asCurrency(gamblePrice)}`
+			);
+			return;
+		}
+
+		await author.add(-price);
+
+    await channel.send(
+			new MessageEmbed()
+				.setTitle(`Guess a number between 1 and 10 to get a prize of ${reward}!`)
+				.setColor(embedColor)
+				//.setDescription();
+        .setImage(
+					'https://media1.tenor.com/images/467d353f7e2d43563ce13fddbb213709/tenor.gif?itemid=12136175'
+				)
+	},
+};
