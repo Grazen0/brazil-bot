fd77fd4a1427b5b176995cdc29369ecd7fa5a264
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-23 17:11:00 -0400

    this doesn't work

--- src/commands/currency/games/numberguess.js
+++ src/commands/currency/games/numberguess.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageCollector } = require('discord.js');
 const config = require('../../../../config.json');
 const { asCurrency } = require('../../../util/utils');
 
@@ -6,10 +6,12 @@ module.exports = {
 	name: 'number',
 	description: 'Earn more money (doubles your gamble)',
   usage: '(number)',
-	execute: async ({ channel, author, client, args }) => {
+	execute: async ({ channel, author, client, args, message }) => {
 		const bal = await author.getBalance();
     const price = parseInt(args[0]);
     const reward = price*3;
+    const answer = Math.floor(Math.random() * 11);
+    const collector = new MessageCollector(message.channel, m => m.author.id === message.author.id);
 
     if (!args.length) {
   		return channel.send(`You didn't mention your bid`);
@@ -22,16 +24,24 @@ module.exports = {
 			return;
 		}
 
-		//await author.add(-price);
+		await author.add(-price);
 
     await channel.send(
 			new MessageEmbed()
 				.setTitle(`Guess a number between 1 and 10 to get a prize of ${reward-price}!`)
 				.setColor(config.embedColor)
-				.setDescription('I don\'t have anything yet')
-        .setImage(
-					'https://media1.tenor.com/images/467d353f7e2d43563ce13fddbb213709/tenor.gif?itemid=12136175'
-				)
+				.setDescription(answer)
       );
+
+      collector.on('collect', message => {
+	       if (Number.isInteger(message.content)) {
+           if (message.content === answer) {
+             author.add(reward);
+             channel.send(`Correct! You won ${reward-price}!`);
+           } else {
+             return channel.send('better luck next time.')
+           }
+         }
+       });
 	},
 };
