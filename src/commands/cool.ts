28f30579f1759495ce838f4f9c0dbe42473e1706
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-24 22:50:46 -0400

    cool

---
+++ src/commands/cool.ts
@@ -0,0 +1,28 @@
+import { MessageEmbed } from 'discord.js';
+import config from '../config.json';
+
+const cool: Command = {
+	name: 'cool',
+	description: 'Find your cool%',
+	execute: async ({ channel, author }) => {
+    const cool = Math.floor(Math.random() * 100);
+
+    if (cool == 69) {
+      await channel.send(
+  			new MessageEmbed()
+  				.setTitle(`${author.username}'s Cool Percentage`)
+  				.setDescription(`${author.username} is **${cool}%** cool`)
+          .setFooter("nice")
+  				.setColor(config.embedColor)
+  		);
+    } else {
+		await channel.send(
+			new MessageEmbed()
+				.setTitle(`${author.username}'s Cool Percentage`)
+				.setDescription(`${author.username} is **${cool}%** cool`)
+				.setColor(config.embedColor)
+		);}
+	},
+};
+
+export default cool;
