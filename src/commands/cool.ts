e39cfa278756e51bd881a21000d135eef6d8c933
ElCholoGamer <josedanielgrayson@gmail.com>
2020-10-26 20:01:09 -0500

    idk

--- src/commands/cool.ts
+++ src/commands/cool.ts
@@ -5,23 +5,24 @@ const cool: Command = {
 	name: 'cool',
 	description: 'Find your cool%',
 	execute: async ({ channel, author }) => {
-    const cool = Math.floor(Math.random() * 100);
+		const cool = Math.floor(Math.random() * 100);
 
-    if (cool == 69) {
-      await channel.send(
-  			new MessageEmbed()
-  				.setTitle(`${author.username}'s Cool Percentage`)
-  				.setDescription(`${author.username} is **${cool}%** cool`)
-          .setFooter("nice")
-  				.setColor(config.embedColor)
-  		);
-    } else {
-		await channel.send(
-			new MessageEmbed()
-				.setTitle(`${author.username}'s Cool Percentage`)
-				.setDescription(`${author.username} is **${cool}%** cool`)
-				.setColor(config.embedColor)
-		);}
+		if (cool == 69) {
+			await channel.send(
+				new MessageEmbed()
+					.setTitle(`${author.username}'s Cool Percentage`)
+					.setDescription(`${author.username} is **${cool}%** cool`)
+					.setFooter('nice')
+					.setColor(config.embedColor)
+			);
+		} else {
+			await channel.send(
+				new MessageEmbed()
+					.setTitle(`${author.username}'s Cool Percentage`)
+					.setDescription(`${author.username} is **${cool}%** cool`)
+					.setColor(config.embedColor)
+			);
+		}
 	},
 };
 
