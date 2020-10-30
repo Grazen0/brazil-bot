75870aa57f392b0cafe8369bf7b9ff2f1e09d2cf
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-30 16:06:49 -0400

    Any subreddit

---
+++ src/commands/reddit/reddit.ts
@@ -0,0 +1,56 @@
+import { MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
+import config from '../../config.json';
+
+const reddit: Command = {
+	name: 'reddit',
+	description: 'Gets a random post from your specified subreddit',
+	execute: async ({ channel, args }) => {
+
+    if (!args.length) {
+      channel.send("please specify your subreddit.");
+      return;
+    }
+
+    const subreddit = args[0];
+
+		const {
+			data: { children },
+		} = await (
+			await fetch(
+				`https://api.reddit.com/r/${subreddit}/hot.json?sort=top&t=day&limit=50`
+			)
+		).json();
+
+    if (!children) {
+      channel.send("that is not a valid subreddit");
+      return;
+    }
+
+		children.sort(() => Math.random() - 0.5);
+
+		const post = children.find(({ data: { over_18 } }: any) => !over_18);
+
+		if (!post) {
+			channel.send("Couldn't find any posts");
+			return;
+		}
+
+		const {
+			data: { url, ups, num_comments, permalink, title, selftext, author },
+		} = post;
+
+		channel.send(
+			new MessageEmbed()
+				.setTitle(title)
+				.setAuthor(`u/${author}`)
+				.setURL(`https://reddit.com${permalink}`)
+				.setColor(config.embedColor)
+        .setImage(url)
+				.setDescription(selftext)
+				.setFooter(`👍 ${ups} | 💬 ${num_comments}`)
+		);
+	},
+};
+
+export default reddit;
