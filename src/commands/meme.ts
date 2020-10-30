1278c9a778bbe5225ae93090d11377b72cd64bbd
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-30 12:34:17 -0400

    coolio

---
+++ src/commands/meme.ts
@@ -0,0 +1,42 @@
+import { MessageEmbed } from 'discord.js';
+import fetch from 'node-fetch';
+import { shuffle } from '../util/utils';
+import config from '../config.json';
+
+const meme: Command = {
+	name: 'meme',
+	description: 'Gets a random meme from Reddit (r/memes)',
+	execute: async ({ channel }) => {
+		const {
+			data: { children },
+		} = await (
+			await fetch(
+				'https://api.reddit.com/r/memes/hot.json?sort=top&t=day&limit=30'
+			)
+		).json();
+
+		children.sort(() => Math.random() - 0.5)
+
+		const post = children.find(
+			({ data: { over_18, post_hint } }) => !over_18 && post_hint === 'image'
+		);
+
+		if (!post) {
+			channel.send("Couldn't find any juicy memes!");
+			return;
+		}
+
+		const { url, ups, num_comments, permalink, title } = post.data;
+
+		channel.send(
+			new MessageEmbed()
+				.setTitle(title)
+				.setURL(`https://reddit.com${permalink}`)
+				.setColor(config.embedColor)
+				.setImage(url)
+				.setFooter(`👍 ${ups} | 💬 ${num_comments}`)
+		);
+	},
+};
+
+export default meme;
