61e3b31d27a8e90964018371aab9fdfa0f7e2901
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-31 12:00:56 -0400

    Maybe?

--- src/commands/reddit/reddit.ts
+++ src/commands/reddit/reddit.ts
@@ -52,7 +52,7 @@ const reddit: Command = {
 
 		const post = children.find(
 			({ data: { over_18, post_hint } }: any) =>
-				!over_18 && post_hint !== 'video'
+				!over_18 && post_hint !== 'Video'
 		);
 
 		if (!post) {
