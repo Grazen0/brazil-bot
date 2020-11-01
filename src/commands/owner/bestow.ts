d38e8116a34d34e181534ef8a795f1b4202f0250
ElCholoGamer <josedanielgrayson@gmail.com>
2020-10-31 19:56:45 -0500

    Update bestow.ts

--- src/commands/owner/bestow.ts
+++ src/commands/owner/bestow.ts
@@ -5,7 +5,7 @@ const bestow: Command = {
 	name: 'bestow',
 	description: '',
 	aliases: ['give', 'gift'],
-	execute: async ({ author, channel, args, message, guild }) => {
+	execute: async ({ author, channel, args, message, client }) => {
 		if (!config.owners.includes(author.id)) {
 			channel.send("You don't have permission for this!");
 			return;
@@ -13,27 +13,27 @@ const bestow: Command = {
 
 		const [id, amount] = args;
 		const reward = parseInt(amount);
-		const member = id.match(/^[0-9]{18}$/)
-			? guild.members.cache.get(id)
-			: message.mentions.members?.first();
+		const user = id.match(/^[0-9]{18}$/)
+			? client.users.cache.get(id)
+			: message.mentions.users?.first();
 
 		if (isNaN(reward)) {
 			channel.send('Please add a valid number');
 			return;
 		}
 
-		if (!member) {
+		if (!user) {
 			channel.send('Please mention a user');
 			return;
 		}
 
-		if (member.bot) {
+		if (user.bot) {
 			channel.send('That user is a bot');
 			return;
 		}
 
-		await member.user.add(reward);
-		channel.send(`${member} received \`${asCurrency(reward)}\``);
+		await user.add(reward);
+		channel.send(`${user.tag} received \`${asCurrency(reward)}\``);
 	},
 };
 
