7c740e6e1b9f95a44641f05e65611ada8c930e31
ElCholoGamer <josedanielgrayson@gmail.com>
2020-11-02 14:53:10 -0500

    test 2

--- src/commands/brazil/setup.ts
+++ src/commands/brazil/setup.ts
@@ -5,15 +5,18 @@ const setup: Command = {
 	name: 'setup',
 	description: 'Setup bot in server',
 	usage: ['setup'],
-	execute: async ({ channel, args }) => {
+	execute: async ({ channel, args, guild }) => {
 		const channelRegex = /^[0-9]{18}$|(?<=^<#)[0-9]{18}(?=>$)/;
 		const roleRegex = /^[0-9]{18}$|(?<=^<@&)[0-9]{18}(?=>$)/;
 
-		const channelId = channelRegex.exec(args[0]);
-		const roleId = roleRegex.exec(args[1]);
+		const [channelId] = channelRegex.exec(args[0]) || [''];
+		const [roleId] = roleRegex.exec(args[1]) || [''];
 
-		if (!channel || !roleId) {
-			channel.send('You must mention a channel and a role!');
+		const brazilChannel = guild.channels.cache.get(channelId);
+		const brazilRole = guild.roles.cache.get(roleId);
+
+		if (!brazilChannel || !brazilRole) {
+			channel.send('You must mention a valid channel and role!');
 			return;
 		}
 
