dd06d81957d5b35498163d2ce550275db2711d49
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-11-02 14:30:08 -0500

    Oof

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -10,7 +10,7 @@ const setup: Command = {
 const regexchannel = /^[0-9]{18}$|(?<=^<#)[0-9]{18}(?=>$)/;
 const regexrole = /^[0-9]{18}$|(?<=^<@&)[0-9]{18}(?=>$)/;
 
-const ids = (args.length && args[0] === regex && args[1] === regex) ?  `Channel: ${args[0]} Role: ${args[1]}`: "You didn't mention something.";
+const ids = (args.length && args[0] === regexchannel && args[1] === regexrole) ?  `Channel: ${args[0]} Role: ${args[1]}`: "You didn't mention something.";
 
 		await channel.send(
 			new MessageEmbed()
