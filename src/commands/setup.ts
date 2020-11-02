392b9c3f359cec26591cb9e3443a8f9ec1f6f28f
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-11-01 20:37:59 -0500

    base command

---
+++ src/commands/setup.ts
@@ -0,0 +1,21 @@
+import { MessageEmbed } from 'discord.js';
+import config from '../config.json';
+
+const setup: Command = {
+	name: 'setup',
+	description: 'Setup bot in server',
+	usage: ['setup'],
+	execute: async ({ channel, message, client }) => {
+
+const ids = (args.length && args[0] === 'oof' && args[1] === 'oof') ?  `Channel: ${args[0]} Role: ${args[1]}`: "You didn't mention anything.";
+
+		await channel.send(
+			new MessageEmbed()
+				.setTitle('Setup')
+				.setDescription(ids)
+				.setColor(config.embedColor)
+		);
+	},
+};
+
+export default setup;
